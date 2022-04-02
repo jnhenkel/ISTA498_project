0b326b889c54bba3e722bfddeda0028a1d12ba78
jnhenkel <jeffnhenkel@gmail.com>
2022-04-02 11:42:52 -0700

    resize

--- src/components/draw.js
+++ src/components/draw.js
@@ -3,7 +3,7 @@ import React from "react";
 const Draw = () => {
     return (
         <div className="container drawDiv">
-        <iframe className="drawFrame" src="https://editor.p5js.org/jnhenkel/full/06l0h3imz" height="440px" width="400px"></iframe>
+        <iframe className="drawFrame" src="https://editor.p5js.org/jnhenkel/full/06l0h3imz" height="540px" width="500px"></iframe>
         </div>
     )
 }
